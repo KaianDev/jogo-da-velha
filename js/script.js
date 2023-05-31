16de0d65187f7330a16a6ee4b8f78c6c3bec4e73
Kaian Vasconcelos <123319433+KaianDev@users.noreply.github.com>
2023-05-31 13:43:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-/* Inital Data
-------------------------------- */
-let playing = false;
-let player = '';
-let warning = '';
+//Initial Data
+let o = `<i class="fa-regular fa-circle fa-lg text-danger"></i>`;
+let x = `<i class="fa-regular fa-x fa-lg text-secondary"></i>`;
+
 let square = {
     a1:'',a2:'',a3:'',
     b1:'',b2:'',b3:'',
-    c1:'',c2:'',c3:''
-};
+    c1:'',c2:'',c3:'',
+}
 
-/* Functions
-------------------------------- */
-function reset () {
+let player = '';
+let playing = false;
+let warning = '';
+
+
+//Functions 
+function reset() {
+    
     warning = '';
-    let random = Math.floor(Math.random() * 2);
-    player = (random === 0) ? 'x' : 'o';
     playing = true;
-
+    let random = Math.floor(Math.random() * 2);
+    player = (random === 0) ? x : o ;
+    
     for(let i in square) {
         square[i] = '';
     }
@@ -26,47 +30,52 @@ function reset () {
 }
 
 function renderInfo() {
-    document.querySelector('.vez').innerHTML = player;
-    document.querySelector('.resultado').innerHTML = warning;
+    document.querySelector('#jogador').innerHTML = player;
+    document.querySelector('#resultado').innerHTML = warning;
 }
 
 function renderSquare() {
-    for(let i in square){
+    for(let i in square) {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
     checkGame();
+    
 }
 
-function clickItem(event) {
+function playClick (event) {
     let item = event.target.getAttribute('data-item');
-    if(square[item] == '' && playing){
+
+    if(square[item] === '' & playing) {
         square[item] = player;
+        
         renderSquare();
         togglePlayer();
     }
+
 }
 
+
 function togglePlayer() {
-    player = (player === 'x') ? 'o' : 'x';
+    player = (player === x) ? o : x;
     renderInfo();
 }
 
 function checkGame() {
-    if(playerWinner('x')){
-        warning = '"X" venceu!';
+    if(winnerIs(x)){
+        warning = `O jogador ${x} venceu`;
         playing = false;
-    } else if(playerWinner('o')){
-        warning = '"O" venceu!';
+    } else if(winnerIs(o)){
+        warning = `O jogador ${o} venceu`;
         playing = false;
-    } else if (isFull()){
-        warning = 'Empatou!';
+    } else if (isFull()) {
+        warning = 'O jogo Empatou!'
         playing = false;
     }
 }
 
-
-function playerWinner(player) {
+function winnerIs(player) {
     let pos = [
         'a1,a2,a3',
         'b1,b2,b3',
@@ -78,17 +87,21 @@ function playerWinner(player) {
 
         'a1,b2,c3',
         'a3,b2,c1'
-    ];
+    ]
+
 
     for(let i in pos) {
+
         let posArray = pos[i].split(',');
 
-        let hasWon = posArray.every(option => square[option] == player);
-        if(hasWon){
+        let hasWon = posArray.every((option) => square[option] == player);
+
+        if(hasWon) {
             return true;
         }
     }
-    return false;    
+
+    return false;
 }
 
 function isFull() {
@@ -100,10 +113,11 @@ function isFull() {
     return true;
 }
 
-/* Events
-------------------------------- */
-document.querySelector('.reset').addEventListener('click',reset);
-document.querySelectorAll('.item').forEach(item => {
-    item.addEventListener('click',clickItem)
-})
+//Events
+document.querySelector('#reset').addEventListener('click',reset);
+document.querySelectorAll('.area-click').forEach((area) => {
+    area.addEventListener('click', playClick)
+});
+
 reset();
+
